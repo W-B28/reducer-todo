b737c54b4c4bbc1b769c70e73411e707e88e3ddc
William Bell <wtb2804@gmail.com>
2020-04-13 19:54:30 -0400

    MVP

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,34 @@
+export const initialState = {
+    todo: { item: "", completed: false },
+    todos: []
+  };
+
+  export const reducer = (state, action) => {
+      console.log(action);
+      //
+      // console.log("this is state", state);
+
+    switch (action.type) {
+      case "ADD_TODO":
+        return {
+          ...state,
+          todos: [...state.todos, { item: action.payload, id: Date.now() }]
+        };
+
+      case "TOGGLE_COMPLETED":
+        return {
+          ...state,
+          todos: state.todos.map(elem =>
+            elem.id === action.payload ? { ...elem, completed: true } : elem
+          )
+        };
+      case "CLEAR_COMPLETED":
+        return {
+          ...state,
+          todos: state.todos.filter(elem => elem.completed !== true)
+        };
+
+      default:
+        return state;
+    }
+  };
